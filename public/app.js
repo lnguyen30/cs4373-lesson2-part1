45a2d4ec99ab09a2d92e9597648cf9bf4045694c
Long Nguyen <lnguyen30@uco.edu>
2021-05-16 17:03:45 -0500

    route configurations

--- public/app.js
+++ public/app.js
@@ -1,1 +1,8 @@
-console.log('app.js')
+import * as Route from './controller/route.js'
+
+window.onload = ()=>{
+    const pathname = window.location.pathname;
+    const hash = window.location.hash;
+
+    Route.routing(pathname, hash);
+}

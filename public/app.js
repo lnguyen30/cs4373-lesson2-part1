503dbfc1488b05864f7501403b1c32a6b92b9cea
Long Nguyen <lnguyen30@uco.edu>
2021-05-18 23:19:55 -0500

    home page mock up

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 import * as Route from './controller/route.js'
 import * as Auth from './controller/auth.js'
 import * as ProductPage from './viewpage/product_page.js'
+import * as Home from './viewpage/home_page.js'
 
 window.onload = ()=>{
     const pathname = window.location.pathname;
@@ -11,3 +12,4 @@ window.onload = ()=>{
 
 Auth.addEventListeners();
 ProductPage.addEventListeners();
+Home.addEventListeners();
